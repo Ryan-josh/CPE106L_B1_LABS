8686327fce426692eaa653f20dfb337f0c5744ad
moonman <160861899+Ryan-josh@users.noreply.github.com>
2024-04-27 23:25:39 +0800

    Update stats.py

--- Lab2/Programming Exercise 1/stats.py
+++ Lab2/Programming Exercise 1/stats.py
@@ -1,1 +1,21 @@
+# stats.py
 
+def median(numbers):
+    numbers.sort()
+    length = len(numbers)
+    if length % 2 == 0:
+        return (numbers[length // 2 - 1] + numbers[length // 2]) / 2
+    else:
+        return numbers[length // 2]
+
+
+def mode(numbers):
+    from collections import Counter
+
+    counter = Counter(numbers)
+    max_count = max(counter.values())
+    return [num for num, count in counter.items() if count == max_count]
+
+
+def mean(numbers):
+    return sum(numbers) / len(numbers)
